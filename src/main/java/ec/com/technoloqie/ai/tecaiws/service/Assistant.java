c845cf5284106def187829b54212e121a39660e7
diegomac <dijavaji@gmail.com>
2024-10-20 17:58:04 -0500

    agrego cambios agente

--- src/main/java/ec/com/technoloqie/ai/tecaiws/service/Assistant.java
+++ src/main/java/ec/com/technoloqie/ai/tecaiws/service/Assistant.java
@@ -3,7 +3,9 @@ package ec.com.technoloqie.ai.tecaiws.service;
 import dev.langchain4j.service.MemoryId;
 import dev.langchain4j.service.SystemMessage;
 import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.V;
 import dev.langchain4j.service.spring.AiService;
+import ec.com.technoloqie.ai.tecaiws.commons.SentimentEnum;
 
 @AiService
 public interface Assistant {
@@ -15,4 +17,15 @@ public interface Assistant {
 	
 	@SystemMessage("Eres un asistente para tareas de respuesta a preguntas. Si no sabes la respuesta, simplemente menciona que no la sabes.")
 	String chat(@MemoryId String memoryId, @UserMessage String userMessage);
+	
+	@SystemMessage("You are a professional translator into {{language}}")
+	@UserMessage("Translate the following text: {{text}}")
+	String translate(@V("text")String text, @V("language")String language);
+	
+	@UserMessage("Analyze sentiment of {{it}}") 	//utiliza it si solo es una palabra
+	SentimentEnum analyzeSentimentOf(String text);
+	
+	@UserMessage("Does {{it}} have a positive sentiment?")
+	Boolean isPositive(String text);
+	
 }
--- src/main/java/ec/com/technoloqie/ai/tecaiws/service/CustomerSupportAgent.java
+++ src/main/java/ec/com/technoloqie/ai/tecaiws/service/CustomerSupportAgent.java
@@ -21,13 +21,12 @@ public interface CustomerSupportAgent {
 	TokenStream chat(@MemoryId String memoryId, @UserMessage String userMessage);
 	
 	@SystemMessage("""
-            You are a customer chat support agent of a car rental company named 'Autoparte los Mochos '
+			You are a customer chat support agent of a car rental company named 'Autoparte los Mochos '
 			Respond in a friendly, helpful, and joyful manner.
 			Before providing information about a booking or cancelling a booking,
 			you MUST always get the following information from the user:
 			booking number, customer first name and last name.
 			Before changing a booking you MUST ensure it is permitted by the terms.
-			Today is {{current_date}}
-            """)
+			Today is {{current_date}} """)
 	TokenStream chat(@MemoryId int memoryId, @UserMessage String userMessage);
 }
