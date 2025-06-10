f66ff896e18b4943c11eb8527ee96f9246cfb4f5
diegomac <dijavaji@gmail.com>
2025-06-10 12:24:39 -0500

    cambio version spring ai

--- src/main/java/ec/com/technoloqie/ai/tecaiws/config/AIConfig.java
+++ src/main/java/ec/com/technoloqie/ai/tecaiws/config/AIConfig.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
 @Configuration
 public class AIConfig {
 	
